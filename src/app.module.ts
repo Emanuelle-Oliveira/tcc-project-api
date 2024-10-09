b051c39d75e7322c2625d651b94331cdc5b4bc58
Emanuelle Oliveira <emanuelletrindadeoliveira@gmail.com>
2024-10-08 21:30:28 -0300

    feat: keycloak auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,44 @@ import { XtableModule } from './xtable/xtable.module';
 import { XcolumnModule } from './xcolumn/xcolumn.module';
 import { RelationshipModule } from './relationship/relationship.module';
 import { QueryModule } from './query/query.module';
+import { RelatedKeysModule } from './related-keys/related-keys.module';
+import {
+  AuthGuard,
+  KeycloakConnectModule,
+  RoleGuard,
+} from 'nest-keycloak-connect';
+import { KeycloakConfigService } from './infra/keycloak-config-service';
+import { APP_GUARD } from '@nestjs/core';
+import { KeycloakConfigModule } from './infra/keycloack-config.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UserModule, ProjectModule, XtableModule, XcolumnModule, RelationshipModule, QueryModule],
+  imports: [
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakConfigService,
+      imports: [KeycloakConfigModule],
+    }),
+    ConfigModule.forRoot(),
+    PrismaModule,
+    UserModule,
+    ProjectModule,
+    XtableModule,
+    XcolumnModule,
+    RelationshipModule,
+    QueryModule,
+    RelatedKeysModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/infra/keycloak-config-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import {
+  KeycloakConnectOptions,
+  KeycloakConnectOptionsFactory,
+  PolicyEnforcementMode,
+  TokenValidation,
+} from 'nest-keycloak-connect';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class KeycloakConfigService implements KeycloakConnectOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createKeycloakConnectOptions(): KeycloakConnectOptions {
+    return {
+      authServerUrl: this.configService.get<string>('AUTH_SERVER_URL'),
+      realm: this.configService.get<string>('REALM'),
+      clientId: this.configService.get<string>('CLIENT_ID'),
+      secret: this.configService.get<string>('SECRET'),
+      cookieKey: 'KEYCLOAK_JWT',
+      logLevels: ['verbose'],
+      useNestLogger: false,
+      policyEnforcement: PolicyEnforcementMode.PERMISSIVE,
+      tokenValidation: TokenValidation.ONLINE,
+    };
+  }
+}
